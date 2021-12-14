03922458494d131e1caa8ed1fa27422cd9b05dd8
Azki Rasyid <73322974+azkira@users.noreply.github.com>
2021-12-14 14:15:29 +0700

    commit

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=['GET'])  
+def home():
+    return 'udhlah'
+    
+@app.route("/anjing", methods=['GET'])  
 def hello_world():
     return {
     "message": "anjing luh bre"
